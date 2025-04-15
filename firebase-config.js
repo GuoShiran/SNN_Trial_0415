817c99b21ad0f00219ea2eddea6c1f223e8256f7
GuoShiran <guoshran@outlook.com>
2025-04-15 19:44:18 +0800

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+// 替换为你的Firebase配置
+const firebaseConfig = {
+    apiKey: "YOUR_API_KEY",
+    authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
+    projectId: "YOUR_PROJECT_ID",
+    storageBucket: "YOUR_PROJECT_ID.appspot.com",
+    messagingSenderId: "YOUR_SENDER_ID",
+    appId: "YOUR_APP_ID"
+};
+
+// 初始化Firebase
+firebase.initializeApp(firebaseConfig);
